e554d6dd6eb8a301221f5b5bb9c2e3e2e4817526
Kashish-0111 <kashishdhankar57@gmail.com>
2025-11-22 17:08:44 +0530

    feat: restructure backend with new app and database connection setup

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -6,7 +6,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB= async () => {
     try {
        const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-       console.log(`/n MonoDB connected !! DB HOST :${  connectionInstance.connection.host}`)
+       console.log(` MonoDB connected !! DB HOST :${  connectionInstance.connection.host}`)
     } catch (error) {
         console.log("MONGODB connection error", error);
         process.exit(1)
